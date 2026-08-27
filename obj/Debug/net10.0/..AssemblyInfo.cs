d18501bfbbd14437ce68752b62fd6a4d945cef31
Gurucharan M <maripalagurucharan@gmail.com>
2026-08-27 20:30:51 +0530

    added connString debug

--- obj/Debug/net10.0/..AssemblyInfo.cs
+++ obj/Debug/net10.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfafddda09f5277536439d82bd08bde416d4c856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61683e302fbf37b76fc82a77c3a10f8650c5f8c0")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
