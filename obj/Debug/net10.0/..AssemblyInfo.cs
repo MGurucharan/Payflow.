7a051c8cf62cc776ebbd8712816f6793d247c19b
Gurucharan M <maripalagurucharan@gmail.com>
2026-09-01 20:07:25 +0530

    modified connection string

--- obj/Debug/net10.0/..AssemblyInfo.cs
+++ obj/Debug/net10.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11797bf1879b9c4d6c6e3fe0b9edb5def719c31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a5ef94415a2607f4a99b6292df4b602beea9a3")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
